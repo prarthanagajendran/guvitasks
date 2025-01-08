dd3d1d5ef8e0f3945c32d8f1a88873537192fa4b
prarthanagajendran <prarthanagajendran@gmail.com>
2025-01-08 18:32:14 +0530

    changes made

--- Fifthquestion.java
+++ Fifthquestion.java
@@ -3,24 +3,31 @@ package javabasics;
 import java.util.Scanner;
 
 public class Fifthquestion {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		 Scanner myObj=new Scanner(System.in);
 			
 		   System.out.println("Enter a number:");
 		   int num =myObj.nextInt();
+		  
+		   	int count=0;//check whether a number is prime or not
+		                  
+		   	for(int i=1;i<=num;i++)
+		   	{
+		   	   if(num%i==0)
+		   	   {
+		   	        count++;
+		   	        
+		   	   }
+		   	}
+		   	if(count==2)
+		   	       System.out.println("prime number ");
+		   	else
+		   	System.out.println("Not a prime number ");	        
+		   	}
+		   
+		   
+	}
 
-       
-        int a = 1;//finding factorial of a given number
-        for(int i = 1; i <= num; i++)
-        {
-           
-           a *= i;
-        }
-        System.out.println(a);
-    }
-			}
-		
-	
+
---
+++ Sixthquestion1.java
@@ -0,0 +1,26 @@
+package javabasics;
+
+import java.util.Scanner;
+
+public class Sixthquestion1 {
+	
+	
+	public static void main(String[] args) {
+		Scanner myObj=new Scanner(System.in);
+		
+		   System.out.println("Enter a number:");
+		   int num =myObj.nextInt();
+
+       
+        int a = 1;//finding factorial of a given number
+        for(int i = 1; i <= num; i++)
+        {
+           
+           a *= i;
+        }
+		 
+        System.out.println(a);
+    }
+			}
+		
+	
